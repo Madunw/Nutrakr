f43b317280e1e961a07991c18533ba85e1ea5c85
Madunw <myegg0517@gmail.com>
2021-09-07 03:17:05 +0900

    layout

---
+++ src/components/Charts/ChartCalorie.vue
@@ -0,0 +1,29 @@
+<template>
+  <ve-liquidfill :data="chartData" height="240px" width="240px" :settings="chartSettings"></ve-liquidfill>
+</template>
+
+<script>
+  export default {
+    name:'ChartCalorie',
+    data () {
+      this.chartSettings = {
+        wave: [0.5, 0.3, 0.1],
+        seriesMap: {
+          '上海': {
+            color: ['red', 'green', 'yellow'],
+            radius: '30%',
+          }
+        }
+      }
+      return {
+        chartData: {
+          columns: ['city', 'percent'],
+          rows: [{
+            city: '上海',
+            percent: 0.7
+          }]
+        }
+      }
+    }
+  }
+</script>
---
+++ src/components/Charts/ChartMacronutrients.vue
@@ -0,0 +1,19 @@
+<template>
+ 
+    <ve-ring :data="totalValue" :extend="extend" height="240px" width="240px" :settings="{radius: [30, 50], offsetY: 100}"></ve-ring>
+  
+</template>
+
+<script>
+export default {
+    name:'ChartMacronutrients',
+    props: {
+        totalValue:{
+            type:Object
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
